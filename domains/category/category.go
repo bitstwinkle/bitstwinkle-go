42721a4c113ff4088c622971460dfd6a95a4e749
淑尤 <suuyoo@yeah.net>
2023-08-06 21:16:44 +0800

    implement category @XIHU

--- domains/category/category.go
+++ domains/category/category.go
@@ -23,6 +23,7 @@ import (
 	"github.com/bitstwinkle/bitstwinkle-go/types/load"
 	"github.com/bitstwinkle/bitstwinkle-go/types/ref"
 	"github.com/bitstwinkle/bitstwinkle-go/types/view/media"
+	"time"
 )
 
 const Ref = "CATEGORY"
@@ -39,21 +40,21 @@ func GetCollar(categoryID ID) ref.Collar {
 const ROOT ID = "$"
 
 type Category struct {
-	Scope ref.Scope `json:"scope"` //所属业务域
-	Lead  ref.Lead  `json:"lead"`  //业务链接: 唯一
-	ID    ID        `json:"id"`    //抽象类目ID
+	Scope ref.Scope `bson:"scope" json:"scope"` //所属业务域
+	Lead  ref.Lead  `bson:"lead" json:"lead"`   //业务链接: 唯一
+	ID    ID        `bson:"id" json:"id"`       //抽象类目ID
 	Tree  struct {
-		Stair  int      `json:"stair"`  //所属层级
-		Parent string   `json:"parent"` //父亲ID
-		Path   []string `json:"path"`   //全路径
+		Stair  int      `bson:"stair" json:"stair"`   //所属层级
+		Parent string   `bson:"parent" json:"parent"` //父亲ID
+		Path   []string `bson:"path" json:"path"`     //全路径
 	} `json:"tree"` //层级结构
-	Name       string     `json:"name"`        //类目名称
-	Media      media.More `json:"media"`       //更多图视频
-	Info       more.More  `json:"info"`        //更多信息
-	Ctrl       *ctrl.Ctrl `json:"ctrl"`        //控制信息
-	Seq        int64      `json:"seq"`         //在上级类目中的排序
-	BrithAt    string     `json:"brith_at"`    //创建时间
-	ModifiedAt string     `json:"modified_at"` //最后修改时间
+	Name       string     `bson:"name" json:"name"`               //类目名称
+	Media      media.More `bson:"media" json:"media"`             //更多图视频
+	Info       more.More  `bson:"info" json:"info"`               //更多信息
+	Ctrl       *ctrl.Ctrl `bson:"ctrl" json:"ctrl"`               //控制信息
+	Seq        int64      `bson:"seq" json:"seq"`                 //在上级类目中的排序
+	BrithAt    time.Time  `bson:"brith_at" json:"brith_at"`       //创建时间
+	ModifiedAt time.Time  `bson:"modified_at" json:"modified_at"` //最后修改时间
 }
 
 type CreateRequest struct {
@@ -86,23 +87,23 @@ type SetRequest struct {
 }
 
 type GetRequest struct {
-	By         load.ByCode `json:"by"`          //BY: category_id|lead
-	CategoryID ID          `json:"category_id"` //[category_id]
-	Lead       *ref.Lead   `json:"lead"`        //[lead]
+	By         load.ByCode `bson:"by" json:"by"`                   //BY: category_id|lead
+	CategoryID ID          `bson:"category_id" json:"category_id"` //[category_id]
+	Lead       *ref.Lead   `bson:"lead" json:"lead"`               //[lead]
 }
 
 type LoadRequest struct {
-	Scope     ref.Scope        `json:"scope"`               //[*]所属业务域
-	CtrlTag   []string         `json:"ctrl_tag,omitempty"`  //控制标
-	Available *ctrl.BooleanSet `json:"available,omitempty"` //是否只返回有效,默认true
-	Keyword   *ctrl.StringSet  `json:"keyword,omitempty"`   //关键词信息
-	Page      *load.Page       `json:"page"`                //分页信息
+	Scope     ref.Scope        `bson:"scope" json:"scope"`         //[*]所属业务域
+	CtrlTag   []string         `bson:"ctrl_tag" json:"ctrl_tag"`   //控制标
+	Available *ctrl.BooleanSet `bson:"available" json:"available"` //是否只返回有效,默认true
+	Keyword   *ctrl.StringSet  `bson:"keyword" json:"keyword"`     //关键词信息
+	Page      *load.Page       `bson:"page" json:"page"`           //分页信息
 }
 
 type Service interface {
 	Create(req CreateRequest) (*Category, *errors.Error)
 	Set(req SetRequest) (*Category, *errors.Error)
 	Get(req GetRequest) (*Category, *errors.Error)
-	Remove(CategoryID string) *errors.Error
+	Remove(categoryID string) *errors.Error
 	Load(req LoadRequest) ([]*Category, *load.Paging, *errors.Error)
 }
